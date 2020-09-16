ea83d8403178f8834a2f4879954311190933cc64
matdon90 <mdonhefner@gmail.com>
2020-09-16 22:35:02 +0200

    Dtos created. Controller created. ListEventQuery and Handler created.

---
+++ SK.Application/Events/Queries/AttendeeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SK.Application.Events.Queries
+{
+    public class AttendeeDto
+    {
+        public string Username { get; set; }
+        public string Image { get; set; }
+        public bool IsHost { get; set; }
+    }
+}
