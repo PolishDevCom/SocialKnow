672fce0d2e46c76574e1ed41f67ee7d93cffabe1
matdon90 <matdon90@DESKTOP-IM1U1S8>
2020-09-17 12:35:40 +0200

    Events seeding. Adding event details logic.

--- SK.API/Controllers/EventsController.cs
+++ SK.API/Controllers/EventsController.cs
@@ -1,6 +1,25 @@
-﻿namespace SK.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using SK.Application.Events.Queries;
+using SK.Application.Events.Queries.DetailsEvent;
+using SK.Application.Events.Queries.ListEvent;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SK.API.Controllers
 {
     public class EventsController : ApiController
     {
+        [HttpGet]
+        public async Task<ActionResult<List<EventDto>>> List()
+        {
+            return await Mediator.Send(new ListEventQuery());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EventDto>> Details(Guid id)
+        {
+            return await Mediator.Send(new DetailsEventQuery { Id = id });
+        }
     }
 }
---
+++ SK.Application/Events/Queries/DetailsEvent/DetailsEventQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace SK.Application.Events.Queries.DetailsEvent
+{
+    public class DetailsEventQuery : IRequest<EventDto>
+    {
+        public Guid Id { get; set; }
+    }
+}
