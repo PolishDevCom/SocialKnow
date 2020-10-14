d0523e3cb93c8d3a1eb5463820968c561476e57e
matdon90 <matdon90@DESKTOP-IM1U1S8>
2020-10-14 16:10:38 +0200

    Testing of adding role to user and removing from user.

---
+++ SK.Application.IntegrationTests/User/Commands/RemoveRoleFromUserTests.cs
@@ -0,0 +1,102 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SK.Application.Common.Exceptions;
+using SK.Application.User.Commands.AddRoleToUser;
+using SK.Application.User.Commands.RegisterUser;
+using SK.Application.User.Commands.RemoveRoleFromUser;
+using System.Threading.Tasks;
+
+namespace SK.Application.IntegrationTests.User.Commands
+{
+    using static Testing;
+    public class RemoveRoleFromUserTests : TestBase
+    {
+        [Test]
+        public async Task ShouldRemoveRoleFromUser()
+        {
+            //arrange
+            await SeedRoles();
+            var registerUserCommand = new RegisterUserCommand()
+            {
+                Username = "Scott101",
+                Email = "scott@localhost",
+                Password = "Pa$$w0rd!"
+            };
+            var user = await SendAsync(registerUserCommand);
+
+            var addRoleCommand = new AddRoleToUserCommand()
+            {
+                Role = "Premium",
+                Username = user.Username
+            };
+            await SendAsync(addRoleCommand);
+
+            var removeRoleCommand = new RemoveRoleFromUserCommand()
+            {
+                Role = "Premium",
+                Username = user.Username
+            };
+
+
+            //act
+            await SendAsync(removeRoleCommand);
+            var result = await GetRolesForUserAsync(user.Username);
+
+            //assert
+            result.Should().HaveCount(1);
+            result.Should().NotContain("Premium");
+        }
+
+        [Test]
+        public async Task ShouldThrowExceptionWhenWrongRole()
+        {
+            //arrange
+            await SeedRoles();
+            var registerUserCommand = new RegisterUserCommand()
+            {
+                Username = "Scott101",
+                Email = "scott@localhost",
+                Password = "Pa$$w0rd!"
+            };
+            var user = await SendAsync(registerUserCommand);
+
+            var removeRoleCommand = new RemoveRoleFromUserCommand()
+            {
+                Role = "NotExistingRole",
+                Username = user.Username
+            };
+
+            //act
+
+            //assert
+            FluentActions.Invoking(() =>
+                SendAsync(removeRoleCommand)).Should().Throw<RestException>();
+        }
+
+        [Test]
+        public async Task ShouldThrowExceptionWhenWrongUser()
+        {
+            //arrange
+            await SeedRoles();
+            var registerUserCommand = new RegisterUserCommand()
+            {
+                Username = "Scott101",
+                Email = "scott@localhost",
+                Password = "Pa$$w0rd!"
+            };
+            var user = await SendAsync(registerUserCommand);
+
+            var removeRoleCommand = new RemoveRoleFromUserCommand()
+            {
+                Role = "Standard",
+                Username = "NotExistingUser"
+            };
+
+            //act
+
+            //assert
+            FluentActions.Invoking(() =>
+                SendAsync(removeRoleCommand)).Should().Throw<NotFoundException>();
+        }
+    }
+}
