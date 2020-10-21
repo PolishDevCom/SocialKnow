68a540d6153054d82667066b2ffe312cd90f477e
matdon90 <mdonhefner@gmail.com>
2020-10-21 07:33:26 +0200

    Added Discussion and Post entity. Configured dbcontext, idbcontext. Added configuration for disscusion to post relation.

---
+++ SK.Domain/Entities/Post.cs
@@ -0,0 +1,14 @@
+﻿using SK.Domain.Common;
+using System;
+
+namespace SK.Domain.Entities
+{
+    public class Post : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Body { get; set; }
+        public bool IsPinned { get; set; }
+        public Guid DiscussionId { get; set; }
+        public virtual Discussion Discussion { get; set; }
+    }
+}
---
+++ SK.Persistence/Configurations/PostConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SK.Domain.Entities;
+
+namespace SK.Persistence.Configurations
+{
+    public class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder
+                .HasOne(p => p.Discussion)
+                .WithMany(d => d.Posts)
+                .HasForeignKey(p => p.DiscussionId);
+        }
+    }
+}
