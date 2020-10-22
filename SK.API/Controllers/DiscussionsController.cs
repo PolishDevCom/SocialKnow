429e6a7fc6fd43f1f8c5e5ce3658d6b0f194e2ba
matdon90 <matdon90@DESKTOP-IM1U1S8>
2020-10-22 09:04:04 +0200

    Discussion creating handler implemented. Added Creat Discussion tests.

---
+++ SK.API/Controllers/DiscussionsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SK.Application.Discussions.Commands.CreateDiscussion;
+using System;
+using System.Threading.Tasks;
+
+namespace SK.API.Controllers
+{
+    public class DiscussionsController : ApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateDiscussionCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
--- SK.Application/Discussions/Commands/CreateDiscussion/CreateDiscussionCommandHandler.cs
+++ SK.Application/Discussions/Commands/CreateDiscussion/CreateDiscussionCommandHandler.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
+using SK.Application.Common.Exceptions;
 using SK.Application.Common.Interfaces;
+using SK.Domain.Entities;
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,17 +12,41 @@ namespace SK.Application.Discussions.Commands.CreateDiscussion
     public class CreateDiscussionCommandHandler : IRequestHandler<CreateDiscussionCommand, Guid>
     {
         private readonly IApplicationDbContext _context;
-        private readonly ICurrentUserService _currentUser;
 
-        public CreateDiscussionCommandHandler(IApplicationDbContext context, ICurrentUserService currentUser)
+        public CreateDiscussionCommandHandler(IApplicationDbContext context)
         {
             _context = context;
-            _currentUser = currentUser;
         }
 
-        public Task<Guid> Handle(CreateDiscussionCommand request, CancellationToken cancellationToken)
+        public async Task<Guid> Handle(CreateDiscussionCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var newDiscussion = new Discussion
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Description = request.Description,
+                IsClosed = false,
+                IsPinned = false
+            };
+
+            await _context.Discussions.AddAsync(newDiscussion);
+
+            var firstPost = new Post
+            {
+                Body = request.PostBody,
+                IsPinned = false,
+                DiscussionId = newDiscussion.Id,
+                Discussion = newDiscussion
+            };
+
+            await _context.Posts.AddAsync(firstPost);
+
+            var success = await _context.SaveChangesAsync(cancellationToken) > 0;
+            if (success)
+            {
+                return newDiscussion.Id;
+            }
+            throw new RestException(HttpStatusCode.BadRequest, new { Event = "Problem saving changes" });
         }
     }
 }
