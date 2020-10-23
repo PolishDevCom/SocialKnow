943888edc9c6892bb274fcad57a2860f2d521486
matdon90 <mdonhefner@gmail.com>
2020-10-23 10:17:43 +0200

    Added command, handler and tests for unpinning discussion. Added unpin discussion action to controller.

---
+++ SK.Application/Discussions/Commands/UnpinDiscussion/UnpinDiscussionCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using SK.Application.Common.Exceptions;
+using SK.Application.Common.Interfaces;
+using SK.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace SK.Application.Discussions.Commands.UnpinDiscussion
+{
+    public class UnpinDiscussionCommandHandler : IRequestHandler<UnpinDiscussionCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UnpinDiscussionCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UnpinDiscussionCommand request, CancellationToken cancellationToken)
+        {
+            var discussionToUnpin = await _context.Discussions.FindAsync(request.Id) ?? throw new NotFoundException(nameof(Discussion), request.Id);
+
+            if (!discussionToUnpin.IsPinned)
+            {
+                throw new RestException(HttpStatusCode.BadRequest, new { Discussion = "Discussion is unpinned already." });
+            }
+
+            discussionToUnpin.IsPinned = false;
+
+            var success = await _context.SaveChangesAsync(cancellationToken) > 0;
+            if (success)
+            {
+                return Unit.Value;
+            }
+            throw new RestException(HttpStatusCode.BadRequest, new { Discussion = "Problem saving changes" });
+        }
+    }
+}
