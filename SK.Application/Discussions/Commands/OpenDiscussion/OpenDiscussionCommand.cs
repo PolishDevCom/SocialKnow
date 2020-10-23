726fd8651d90026db444235f1221ca3c6bf5c6cc
matdon90 <matdon90@DESKTOP-IM1U1S8>
2020-10-23 09:23:03 +0200

    Added command, handler and tests for opening discussion. Added open discussion action to controller.

---
+++ SK.Application/Discussions/Commands/OpenDiscussion/OpenDiscussionCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace SK.Application.Discussions.Commands.OpenDiscussion
+{
+    public class OpenDiscussionCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
