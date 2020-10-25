f33cf6b8e31608ba0f3eb6c8d368bd1a6e67c589
matdon90 <matdon90@DESKTOP-IM1U1S8>
2020-10-25 15:08:58 +0100

    Added command, validator, handler and tests for creating posts in discussion. Added action in posts controller.

---
+++ SK.Application/Posts/Commands/CreatePost/CreatePostCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace SK.Application.Posts.Commands.CreatePost
+{
+    public class CreatePostCommandValidator : AbstractValidator<CreatePostCommand>
+    {
+        public CreatePostCommandValidator()
+        {
+            RuleFor(p => p.Body)
+                .NotEmpty().WithMessage("Post body is required.");
+            RuleFor(p => p.DiscussionId)
+                .NotEmpty().WithMessage("Discussion Id is required.");
+        }
+    }
+}
