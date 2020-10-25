9de1910ad1e2da92a35b9aea64491e57985039e1
matdon90 <mdonhefner@gmail.com>
2020-10-25 15:08:58 +0100

    Added command, validator, handler and tests for creating posts in discussion. Added action in posts controller.

---
+++ SK.API/Controllers/PostsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using SK.Application.Posts.Commands.CreatePost;
+
+namespace SK.API.Controllers
+{
+    public class PostsController : ApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreatePostCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
---
+++ SK.Application/Posts/Commands/CreatePost/CreatePostCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using SK.Application.Common.Exceptions;
+using SK.Application.Common.Interfaces;
+using SK.Domain.Entities;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SK.Application.Posts.Commands.CreatePost
+{
+    public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, Guid>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreatePostCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Guid> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        {
+            var discussionToFind = await _context.Discussions.FindAsync(request.DiscussionId) ?? throw new NotFoundException(nameof(Discussion), request.Id);
+
+            if (discussionToFind.IsClosed)
+            {
+                throw new RestException(HttpStatusCode.BadRequest, new { Post = "Adding post to closed discussion is not allowed." });
+            }
+
+            var newPost = new Post()
+            {
+                Id = request.Id,
+                Body = request.Body,
+                IsPinned = false,
+                DiscussionId = request.DiscussionId
+            };
+
+            await _context.Posts.AddAsync(newPost);
+
+            var success = await _context.SaveChangesAsync(cancellationToken) > 0;
+            if (success)
+            {
+                return newPost.Id;
+            }
+            throw new RestException(HttpStatusCode.BadRequest, new { Event = "Problem saving changes" });
+        }
+    }
+}
