15071fc2a9e8c2a328db4044ee12d4df597ce1d8
matdon90 <mdonhefner@gmail.com>
2020-11-19 08:41:22 +0100

    Added logic for pagination of event listing response. Adapting changes in eventController, query, quryhandler and tests.

--- SK.Application.IntegrationTests/Articles/Queries/ListArticleTests.cs
+++ SK.Application.IntegrationTests/Articles/Queries/ListArticleTests.cs
@@ -18,8 +18,9 @@ namespace SK.Application.IntegrationTests.Articles.Queries
         public async Task ShouldReturnAllArticlesAsAList()
         {
             //arrange
+            int numberOfArticles = 3;
             await RunAsUserAsync("scott101@localhost", "Pa$$w0rd!");
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < numberOfArticles; i++)
             {
                 await AddAsync(new Faker<Article>("en")
                     .RuleFor(a => a.Id, f => f.Random.Guid())
@@ -39,7 +40,16 @@ namespace SK.Application.IntegrationTests.Articles.Queries
             var result = await SendAsync(query);
 
             //assert
-            result.Data.Should().HaveCount(3);
+            result.FirstPage.Should().BeNull();
+            result.LastPage.Should().BeNull();
+            result.NextPage.Should().BeNull();
+            result.PreviousPage.Should().BeNull();
+
+            result.Succeeded.Should().BeTrue();
+            result.Errors.Should().BeNull();
+            result.TotalRecords.Should().Be(numberOfArticles);
+
+            result.Data.Should().HaveCount(numberOfArticles);
         }
     }
 }
