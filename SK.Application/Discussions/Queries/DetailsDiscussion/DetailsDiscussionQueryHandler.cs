855b72925bd033246bb850552b97f5bb1fe2f92c
matdon90 <matdon90@DESKTOP-IM1U1S8>
2020-11-19 13:38:01 +0100

    Added logic for pagination of discussions and posts - changes in controller, queries, and query handlers. Added new Dtos. Added seeding of new discussions with posts. Adapting tests to implemented changes.

--- SK.Application/Discussions/Queries/DetailsDiscussion/DetailsDiscussionQueryHandler.cs
+++ SK.Application/Discussions/Queries/DetailsDiscussion/DetailsDiscussionQueryHandler.cs
@@ -9,22 +9,29 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Collections.Generic;
+using SK.Application.Common.Models;
+using System;
 
 namespace SK.Application.Discussions.Queries.DetailsDiscussion
 {
-    public class DetailsDiscussionQueryHandler : IRequestHandler<DetailsDiscussionQuery, DiscussionDto>
+    public class DetailsDiscussionQueryHandler : IRequestHandler<DetailsDiscussionQuery, DiscussionWithPagedPostsDto>
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IUriService _uriService;
 
-        public DetailsDiscussionQueryHandler(IApplicationDbContext context, IMapper mapper)
+        public DetailsDiscussionQueryHandler(IApplicationDbContext context, IMapper mapper, IUriService uriService)
         {
             _context = context;
             _mapper = mapper;
+            _uriService = uriService;
         }
 
-        public async Task<DiscussionDto> Handle(DetailsDiscussionQuery request, CancellationToken cancellationToken)
+        public async Task<DiscussionWithPagedPostsDto> Handle(DetailsDiscussionQuery request, CancellationToken cancellationToken)
         {
+            var route = request.Path;
+            var validFilter = new PaginationFilter(request.Filter.PageNumber, request.Filter.PageSize);
+
             var selectedDiscussionWithPosts = await _context.Discussions
                 .Include(d => d.Posts)
                 .ProjectTo<DiscussionDto>(_mapper.ConfigurationProvider)
@@ -44,10 +51,53 @@ namespace SK.Application.Discussions.Queries.DetailsDiscussion
             orderedPosts.AddRange(pinnedPosts);
             orderedPosts.AddRange(unpinnedPosts);
 
-            selectedDiscussionWithPosts.Posts = orderedPosts;
-            selectedDiscussionWithPosts.NumberOfPosts = orderedPosts?.Count ?? 0;
 
-            return selectedDiscussionWithPosts;
+            //data for paged posts
+            var pagedData = orderedPosts
+                .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
+                .Take(validFilter.PageSize).ToList();
+            var totalRecords = orderedPosts?.Count ?? 0;
+            int totalPages = Convert.ToInt32(Math.Ceiling(((double)totalRecords / (double)validFilter.PageSize)));
+            var nextPage =
+                validFilter.PageNumber >= 1 && validFilter.PageNumber < totalPages
+                ? _uriService.GetPageUri(new PaginationFilter(validFilter.PageNumber + 1, validFilter.PageSize), route)
+                : null;
+            var previousPage =
+                validFilter.PageNumber - 1 >= 1 && validFilter.PageNumber <= totalPages
+                ? _uriService.GetPageUri(new PaginationFilter(validFilter.PageNumber - 1, validFilter.PageSize), route)
+                : null;
+            var firstPage = _uriService.GetPageUri(new PaginationFilter(1, validFilter.PageSize), route);
+            var lastPage = _uriService.GetPageUri(new PaginationFilter(totalPages, validFilter.PageSize), route);
+
+            var pagedPosts = new PagedPostsDto()
+            {
+                PageNumber = validFilter.PageNumber,
+                PageSize = validFilter.PageSize,
+                FirstsPage = firstPage,
+                LastPage = lastPage,
+                TotalPages = totalPages,
+                TotalPosts = totalRecords,
+                NextPage = nextPage,
+                PreviousPage = previousPage,
+                Data = pagedData
+            };
+
+            var discussionWithPagedPosts = new DiscussionWithPagedPostsDto()
+            {
+                Id = selectedDiscussionWithPosts.Id,
+                Title = selectedDiscussionWithPosts.Title,
+                Description = selectedDiscussionWithPosts.Description,
+                IsPinned = selectedDiscussionWithPosts.IsPinned,
+                IsClosed = selectedDiscussionWithPosts.IsClosed,
+                CreatedBy = selectedDiscussionWithPosts.CreatedBy,
+                Created = selectedDiscussionWithPosts.Created,
+                LastModifiedBy = selectedDiscussionWithPosts.LastModifiedBy,
+                LastModified = selectedDiscussionWithPosts.LastModified,
+                NumberOfPosts = totalRecords,
+                Posts = pagedPosts
+            };
+
+            return discussionWithPagedPosts;
         }
     }
 }
---
+++ SK.Application/Discussions/Queries/DiscussionWithPagedPostsDto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace SK.Application.Discussions.Queries
+{
+    public class DiscussionWithPagedPostsDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsPinned { get; set; }
+        public bool IsClosed { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime Created { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+        public int NumberOfPosts { get; set; }
+        public PagedPostsDto Posts { get; set; }
+    }
+}
