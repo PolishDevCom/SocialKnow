43b5522bfe8b9f303fce80a3d6c2708444e49642
matdon90 <mdonhefner@gmail.com>
2020-12-04 12:21:09 +0100

    Refactoring tests and deleting test that includes other services.

--- SK.Application.IntegrationTests/Photos/Commands/DeletePhoto/DeletePhotoTest.cs
+++ SK.Application.IntegrationTests/Photos/Commands/DeletePhoto/DeletePhotoTest.cs
@@ -12,33 +12,6 @@ namespace SK.Application.IntegrationTests.Photos.Commands.DeletePhoto
     public class DeletePhotoTest : TestBase
     {
         [Test]
-        public async Task ShouldDeletePhoto()
-        {
-            //arrange
-            var creatorUsername = await RunAsUserAsync("scott101@localhost", "Pa$$w0rd!");
-            var photo1 = new Faker<Photo>()
-                .RuleFor(p => p.Id, f => f.Lorem.Text())
-                .RuleFor(p => p.IsMain, f => true)
-                .RuleFor(p => p.Url, f => f.Lorem.Text()).Generate();
-            var photo2 = new Faker<Photo>()
-                .RuleFor(p => p.Id, f => f.Lorem.Text())
-                .RuleFor(p => p.IsMain, f => false)
-                .RuleFor(p => p.Url, f => f.Lorem.Text()).Generate();
-
-            await AddPhotoToUserAsync(photo1, creatorUsername);
-            await AddPhotoToUserAsync(photo2, creatorUsername);
-
-            var deletePhotoCommand = new DeletePhotoCommand(photo2.Id);
-
-            //act
-            await SendAsync(deletePhotoCommand);
-
-            //assert
-            var photo2FromDb = await FindByStringAsync<Photo>(photo2.Id);
-            photo2FromDb.Should().BeNull();
-        }
-
-        [Test]
         public async Task ShouldBeNotFoundExceptionWhenWrongPhototId()
         {
             //arrange
