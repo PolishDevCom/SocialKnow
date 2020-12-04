c8be0cab0a959d534bd938a27b3b706a2d01a6bd
matdon90 <mdonhefner@gmail.com>
2020-12-04 10:30:45 +0100

    Added integration tests for adding, deleting and setting main photo.

---
+++ SK.Application.IntegrationTests/Photos/Commands/AddPhoto/AddPhotoTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using NUnit.Framework;
+using SK.Application.Photos.Commands.AddPhoto;
+using SK.Domain.Entities;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK.Application.IntegrationTests.Photos.Commands.AddPhoto
+{
+    using static Testing;
+    public class AddPhotoTest : TestBase
+    {
+        [Test]
+        public async Task ShouldCreatePhoto()
+        {
+            //arrange
+            var loggedUser = await RunAsUserAsync("scott101@localhost", "Pa$$w0rd!");
+            IFormFile file = new FormFile(new MemoryStream(Encoding.UTF8.GetBytes("This is a dummy file")), 0, 0, "Data", "dummy.txt");
+            var command = new AddPhotoCommand() { File = file };
+
+            //act
+            var createdTestPhoto = await SendAsync(command);
+
+            var createdTestPhotoFromDb = await FindByStringAsync<Photo>(createdTestPhoto.Id);
+
+            //assert
+            createdTestPhotoFromDb.Should().NotBeNull();
+        }
+    }
+}
---
+++ SK.Application.IntegrationTests/Photos/Commands/DeletePhoto/DeletePhotoTest.cs
@@ -0,0 +1,93 @@
+﻿using Bogus;
+using FluentAssertions;
+using NUnit.Framework;
+using SK.Application.Common.Exceptions;
+using SK.Application.Photos.Commands.DeletePhoto;
+using SK.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace SK.Application.IntegrationTests.Photos.Commands.DeletePhoto
+{
+    using static Testing;
+    public class DeletePhotoTest : TestBase
+    {
+        [Test]
+        public async Task ShouldDeletePhoto()
+        {
+            //arrange
+            var creatorUsername = await RunAsUserAsync("scott101@localhost", "Pa$$w0rd!");
+            var photo1 = new Faker<Photo>()
+                .RuleFor(p => p.Id, f => f.Lorem.Text())
+                .RuleFor(p => p.IsMain, f => true)
+                .RuleFor(p => p.Url, f => f.Lorem.Text()).Generate();
+            var photo2 = new Faker<Photo>()
+                .RuleFor(p => p.Id, f => f.Lorem.Text())
+                .RuleFor(p => p.IsMain, f => false)
+                .RuleFor(p => p.Url, f => f.Lorem.Text()).Generate();
+
+            await AddPhotoToUserAsync(photo1, creatorUsername);
+            await AddPhotoToUserAsync(photo2, creatorUsername);
+
+            var deletePhotoCommand = new DeletePhotoCommand(photo2.Id);
+
+            //act
+            await SendAsync(deletePhotoCommand);
+
+            //assert
+            var photo2FromDb = await FindByStringAsync<Photo>(photo2.Id);
+            photo2FromDb.Should().BeNull();
+        }
+
+        [Test]
+        public async Task ShouldBeNotFoundExceptionWhenWrongPhototId()
+        {
+            //arrange
+            var creatorUsername = await RunAsUserAsync("scott101@localhost", "Pa$$w0rd!");
+            var photo1 = new Faker<Photo>()
+                .RuleFor(p => p.Id, f => f.Lorem.Text())
+                .RuleFor(p => p.IsMain, f => true)
+                .RuleFor(p => p.Url, f => f.Lorem.Text()).Generate();
+            var photo2 = new Faker<Photo>()
+                .RuleFor(p => p.Id, f => f.Lorem.Text())
+                .RuleFor(p => p.IsMain, f => false)
+                .RuleFor(p => p.Url, f => f.Lorem.Text()).Generate();
+
+            await AddPhotoToUserAsync(photo1, creatorUsername);
+            await AddPhotoToUserAsync(photo2, creatorUsername);
+
+            var deletePhotoCommand = new DeletePhotoCommand("wrongId");
+
+            //act
+
+            //assert
+            FluentActions.Invoking(() =>
+                SendAsync(deletePhotoCommand)).Should().Throw<NotFoundException>();
+        }
+
+        [Test]
+        public async Task ShouldBeRestExceptionWhenMainId()
+        {
+            //arrange
+            var creatorUsername = await RunAsUserAsync("scott101@localhost", "Pa$$w0rd!");
+            var photo1 = new Faker<Photo>()
+                .RuleFor(p => p.Id, f => f.Lorem.Text())
+                .RuleFor(p => p.IsMain, f => true)
+                .RuleFor(p => p.Url, f => f.Lorem.Text()).Generate();
+            var photo2 = new Faker<Photo>()
+                .RuleFor(p => p.Id, f => f.Lorem.Text())
+                .RuleFor(p => p.IsMain, f => false)
+                .RuleFor(p => p.Url, f => f.Lorem.Text()).Generate();
+
+            await AddPhotoToUserAsync(photo1, creatorUsername);
+            await AddPhotoToUserAsync(photo2, creatorUsername);
+
+            var deletePhotoCommand = new DeletePhotoCommand(photo1.Id);
+
+            //act
+
+            //assert
+            FluentActions.Invoking(() =>
+                SendAsync(deletePhotoCommand)).Should().Throw<RestException>();
+        }
+    }
+}
