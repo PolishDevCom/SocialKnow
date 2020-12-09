49a49e0ac962cec3eb7ccac256d3ae38959336ff
matdon90 <mdonhefner@gmail.com>
2020-12-09 08:50:11 +0100

    Added profiles controller. Changes in query constructor. Added tests.

--- SK.Application/Profiles/Queries/DetailsProfile/DetailsProfileQuery.cs
+++ SK.Application/Profiles/Queries/DetailsProfile/DetailsProfileQuery.cs
@@ -5,6 +5,10 @@ namespace SK.Application.Profiles.Queries.DetailsProfile
 {
     public class DetailsProfileQuery : IRequest<ProfileDto>
     {
+        public DetailsProfileQuery(string username)
+        {
+            Username = username;
+        }
         public string Username { get; set; }
     }
 }
