d4fe3983d5d61779230bee1861862fc5657fcf9a
matdon90 <mdonhefner@gmail.com>
2020-12-28 08:51:21 +0100

    ValidatiomBehaviour renaming.

--- SK.Application/DependencyInjection.cs
+++ SK.Application/DependencyInjection.cs
@@ -16,7 +16,7 @@ namespace SK.Application
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
 
             return services;
