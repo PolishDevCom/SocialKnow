54fd80401ea0d51fe359cdd72541628860caee71
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2021-02-08 22:13:08 +0100

    add links to AppBar

--- sk-web-app/src/components/AppBar.tsx
+++ sk-web-app/src/components/AppBar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { VIEWS_PATH } from "../consts";
 
 const AppBar: React.FC<unknown> = () => {
     return (
         <div>
             <p>
-                AppBar
+                <Link to={`${VIEWS_PATH.HOME}`}>Home</Link> || 
+                <Link to={`${VIEWS_PATH.LOGIN}`}>Login</Link> || 
             </p>
         </div>
     );
---
+++ sk-web-app/src/consts.ts
@@ -0,0 +1,4 @@
+export const VIEWS_PATH = {
+    HOME: "/",
+    LOGIN: "/login",
+};
