4bb5a2f045135ef487ca54a596894584f4c11b62
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2021-02-08 18:04:17 +0100

    remove default content

---
+++ sk-web-app/src/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC<unknown> = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Clear site
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
