ff764ecd4e1683e42da55fe3732966091be5fd05
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2021-02-09 15:01:21 +0100

    add data from fields to states

--- sk-web-app/src/views/Login/Login.tsx
+++ sk-web-app/src/views/Login/Login.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login: React.FC<unknown> = () => {
+    const [userName, setUserName] = useState<String>("");
+    const [password, setPassword] = useState<String>("");
     return (
         <div>
             <p>
-                Username: <input type="text"/>
+                Username: 
+                <input type="text" onChange={event => setUserName(event.target.value)} />
             </p>
             <p>
-                Password: <input type="password"/>
+                Password: 
+                <input type="password" onChange={event => setPassword(event.target.value)}/>
             </p>
             <p>
-                <button>Login</button>
+                <button onClick={() => {
+                    console.log(userName + " " + password);
+                }}>
+                    Login
+                </button>
             </p>
         </div>
     )
