499e6e8f689429559dcaf0b92e01b9fb56b5a4ce
matdon90 <mdonhefner@gmail.com>
2021-03-25 12:13:17 +0100

    Adds Redis configuration.

--- SK.Persistence/DependencyInjection.cs
+++ SK.Persistence/DependencyInjection.cs
@@ -9,12 +9,16 @@ namespace SK.Persistence
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
+            //PostgreSQL configuration
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseLazyLoadingProxies();
                 options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
                     b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
             });
+
+            //Redis configuration
+            services.AddStackExchangeRedisCache(options => options.Configuration = configuration["Redis:ConnectionString"]);
 
             services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
 
