bdbd50e6b017178f95eb1c6648cd6760f2dc1bc3
Patryk Wyprzał <patryk836@o2.pl>
2021-04-01 17:44:32 +0200

    Test DeleteDiscussionCommand.

---
+++ SK.Application.UnitTests/Discussions/Commands/DeleteDiscussionCommandTest.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Moq;
+using NUnit.Framework;
+using SK.Application.Common.Exceptions;
+using SK.Application.Common.Interfaces;
+using SK.Application.Common.Resources.Discussions;
+using SK.Application.Discussions.Commands.DeleteDiscussion;
+using SK.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SK.Application.UnitTests.Discussions.Commands
+{
+    public class DeleteDiscussionCommandTest
+    {
+        private readonly Guid id;
+        private readonly Mock<DbSet<Discussion>> dbSetDiscussion;
+        private readonly Mock<IApplicationDbContext> context;
+        private readonly Mock<IStringLocalizer<DiscussionsResource>> stringLocalizer;
+
+        private readonly Discussion discussion;
+
+        public DeleteDiscussionCommandTest()
+        {
+            id = new Guid();
+            dbSetDiscussion = new Mock<DbSet<Discussion>>();
+            context = new Mock<IApplicationDbContext>();
+            stringLocalizer = new Mock<IStringLocalizer<DiscussionsResource>>();
+
+            discussion = new Discussion { Id = id };
+        }
+
+        [Test]
+        public async Task ShouldCallHandle()
+        {
+            //Arrange
+            dbSetDiscussion.Setup(x => x.FindAsync(id)).Returns(new ValueTask<Discussion>(Task.FromResult(discussion)));
+            context.Setup(x => x.Discussions).Returns(dbSetDiscussion.Object);
+            context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>())).Returns(Task.FromResult(1));
+
+            DeleteDiscussionCommandHandler deleteDiscussionCommandHandler = new DeleteDiscussionCommandHandler(context.Object, stringLocalizer.Object);
+            DeleteDiscussionCommand deleteDiscussionCommand = new DeleteDiscussionCommand(id);
+
+            //Act
+            var result = await deleteDiscussionCommandHandler.Handle(deleteDiscussionCommand, new CancellationToken());
+
+            //Assert
+            result.Should().Be(Unit.Value);
+        }
+
+        [Test]
+        public void ShouldNotCallHandleIfDiscussionNotExist()
+        {
+            //Arrange
+            dbSetDiscussion.Setup(x => x.FindAsync(id)).Returns(null);
+            context.Setup(x => x.Discussions).Returns(dbSetDiscussion.Object);
+
+            DeleteDiscussionCommandHandler deleteDiscussionCommandHandler = new DeleteDiscussionCommandHandler(context.Object, stringLocalizer.Object);
+            DeleteDiscussionCommand deleteDiscussionCommand = new DeleteDiscussionCommand(id);
+
+            //Act
+            Func<Task> act = async () => await deleteDiscussionCommandHandler.Handle(deleteDiscussionCommand, new CancellationToken());
+
+            //Assert
+            act.Should().Throw<NotFoundException>();
+        }
+
+        [Test]
+        public void ShouldNotCallHandleIfNotSavedChanges()
+        {
+            //Arrange
+            dbSetDiscussion.Setup(x => x.FindAsync(id)).Returns(new ValueTask<Discussion>(Task.FromResult(discussion)));
+            context.Setup(x => x.Discussions).Returns(dbSetDiscussion.Object);
+            context.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>())).Returns(Task.FromResult(0));
+
+            DeleteDiscussionCommandHandler deleteDiscussionCommandHandler = new DeleteDiscussionCommandHandler(context.Object, stringLocalizer.Object);
+            DeleteDiscussionCommand deleteDiscussionCommand = new DeleteDiscussionCommand(id);
+
+            //Act
+            Func<Task> act = async () => await deleteDiscussionCommandHandler.Handle(deleteDiscussionCommand, new CancellationToken());
+
+            //Assert
+            act.Should().Throw<RestException>();
+        }
+    }
+}
