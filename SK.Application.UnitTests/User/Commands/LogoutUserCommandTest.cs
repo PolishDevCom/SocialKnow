5d1fec94acf0667f1f1fb286f7689042b6dd8ec6
Patryk Wyprzał <patryk836@o2.pl>
2021-04-04 04:46:13 +0200

    Test LogoutUserCommand.

---
+++ SK.Application.UnitTests/User/Commands/LogoutUserCommandTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SK.Application.Common.Interfaces;
+using SK.Application.User.Commands.LogoutUser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SK.Application.UnitTests.User.Commands
+{
+    public class LogoutUserCommandTest
+    {
+        private readonly Mock<ITokenManager> tokenManager;
+
+        public LogoutUserCommandTest()
+        {
+            tokenManager = new Mock<ITokenManager>();
+        }
+
+        [Test]
+        public async Task ShouldCallHandle()
+        {
+            //Arrange
+            LogoutUserCommandHandler logoutUserCommandHandler = new LogoutUserCommandHandler(tokenManager.Object);
+            LogoutUserCommand logoutUserCommand = new LogoutUserCommand();
+
+            //Act
+            var result = await logoutUserCommandHandler.Handle(logoutUserCommand, new CancellationToken());
+
+            //Assert
+            result.Should().Be(Unit.Value);
+        }
+    }
+}
