4ce5ecaa006e52c79229f7421379a691c9443442
Patryk Wyprzał <patryk836@o2.pl>
2021-04-14 20:28:17 +0200

    Add a new Tag entity.

---
+++ SK.Domain/Entities/Tag.cs
@@ -0,0 +1,13 @@
+﻿using SK.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SK.Domain.Entities
+{
+    public class Tag : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
