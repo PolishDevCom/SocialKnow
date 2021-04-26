c10da452fb88ed121b06fa42393006fd8b577752
Patryk Wyprzał <patryk836@o2.pl>
2021-04-26 19:31:28 +0200

    Use pagination service in queries.

--- SK.Application/Articles/Queries/ListArticle/ListArticleQueryHandler.cs
+++ SK.Application/Articles/Queries/ListArticle/ListArticleQueryHandler.cs
@@ -6,6 +6,7 @@ using SK.Application.Common.Helpers;
 using SK.Application.Common.Interfaces;
 using SK.Application.Common.Models;
 using SK.Application.Common.Wrappers;
+using SK.Domain.Entities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -15,32 +16,18 @@ namespace SK.Application.Articles.Queries.ListArticle
 { 
     public class ListArticleQueryHandler : IRequestHandler<ListArticleQuery, PagedResponse<List<ArticleDto>>>
     {
-        private readonly IApplicationDbContext _context;
-        private readonly IMapper _mapper;
-        private readonly IUriService _uriService;
+        private readonly IPaginationService<Article, ArticleDto> paginationService;
 
-        public ListArticleQueryHandler(IApplicationDbContext context, IMapper mapper, IUriService uriService)
+        public ListArticleQueryHandler(IPaginationService<Article, ArticleDto> paginationService)
         {
-            _context = context;
-            _mapper = mapper;
-            _uriService = uriService;
+            this.paginationService = paginationService;
         }
 
         public async Task<PagedResponse<List<ArticleDto>>> Handle(ListArticleQuery request, CancellationToken cancellationToken)
         {
             var route = request.Path;
             var validFilter = new PaginationFilter(request.Filter.PageNumber, request.Filter.PageSize);
-            var pagedData = await _context.Articles
-                .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
-                .Take(validFilter.PageSize)
-                .ProjectTo<ArticleDto>(_mapper.ConfigurationProvider)
-                .OrderByDescending(a => a.Created)
-                .ToListAsync(cancellationToken);
-            var totalRecords = await _context.Articles.CountAsync();
-            var pagedResponse = PaginationHelper.CreatePagedReponse<ArticleDto>(pagedData, validFilter, totalRecords, _uriService, route);
-
-            return pagedResponse;
-
+            return await paginationService.GetPagedData(validFilter, route, cancellationToken);
         }
     }
 }
--- SK.Application/Events/Queries/ListEvent/ListEventQueryHandler.cs
+++ SK.Application/Events/Queries/ListEvent/ListEventQueryHandler.cs
@@ -6,6 +6,7 @@ using SK.Application.Common.Helpers;
 using SK.Application.Common.Interfaces;
 using SK.Application.Common.Models;
 using SK.Application.Common.Wrappers;
+using SK.Domain.Entities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -15,31 +16,18 @@ namespace SK.Application.Events.Queries.ListEvent
 {
     public class ListEventQueryHandler : IRequestHandler<ListEventQuery, PagedResponse<List<EventDto>>>
     {
-        private readonly IApplicationDbContext _context;
-        private readonly IMapper _mapper;
-        private readonly IUriService _uriService;
+        private readonly IPaginationService<Event, EventDto> paginationService;
 
-        public ListEventQueryHandler(IApplicationDbContext context, IMapper mapper, IUriService uriService)
+        public ListEventQueryHandler(IPaginationService<Event, EventDto> paginationService)
         {
-            _context = context;
-            _mapper = mapper;
-            _uriService = uriService;
+            this.paginationService = paginationService;
         }
 
         public async Task<PagedResponse<List<EventDto>>> Handle(ListEventQuery request, CancellationToken cancellationToken)
         {
             var route = request.Path;
             var validFilter = new PaginationFilter(request.Filter.PageNumber, request.Filter.PageSize);
-            var pagedData = await _context.Events
-                .Skip((validFilter.PageNumber - 1) * validFilter.PageSize)
-                .Take(validFilter.PageSize)
-                .ProjectTo<EventDto>(_mapper.ConfigurationProvider)
-                .OrderByDescending(e => e.Date)
-                .ToListAsync(cancellationToken);
-            var totalRecords = await _context.Events.CountAsync();
-            var pagedResponse = PaginationHelper.CreatePagedReponse<EventDto>(pagedData, validFilter, totalRecords, _uriService, route);
-
-            return pagedResponse;
+            return await paginationService.GetPagedData(validFilter, route, cancellationToken, e => e.Date);
         }
     }
 }
