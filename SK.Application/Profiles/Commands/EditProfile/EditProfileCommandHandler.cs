00d85f2aab2af04aff36ab12e011411e4764b535
matdon90 <mdonhefner@gmail.com>
2021-05-20 22:34:49 +0200

    Adds profile resources. Adds edit profile command and dtos.

---
+++ SK.Application/Profiles/Commands/EditProfile/EditProfileCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SK.Application.Profiles.Commands.EditProfile
+{
+    public class EditProfileCommandHandler : IRequestHandler<EditProfileCommand>
+    {
+        public Task<Unit> Handle(EditProfileCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
